74c2739cb619fc6edab0e5ce7579f33ce6f34105
gustavnybergs <Gustavnybergs@gmail.com>
2024-11-05 23:18:56 +0100

    Update Burglar.java

---
+++ src/main/java/se/gustav/adventureGame/model/Burglar.java
@@ -0,0 +1,10 @@
+package se.gustav.adventureGame.model;
+
+import se.gustav.adventureGame.logic.*;
+import se.gustav.adventureGame.model.*;
+
+public class Burglar extends Entity {
+    public Burglar(String name, int health, int damage) {
+        super(name, health, damage);
+    }
+}
